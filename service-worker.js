c04bd1461d08768cecb5708472f62f8c77a6708a
Mikołaj Ćwikła <62749434+MrWoodsman@users.noreply.github.com>
2024-02-29 00:22:47 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function (event) {
+	// Perform install steps
+	event.waitUntil(
+		caches.open('v1').then(function (cache) {
+			console.log('Opened cache');
+			return cache.addAll([
+				'/style.css',
+				'/index.html',
+				// Add additional resources here
+			]);
+		})
+	);
+});
+
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			// Cache hit - return response
+			if (response) {
+				return response;
+			}
+			return fetch(event.request);
+		})
+	);
+});
